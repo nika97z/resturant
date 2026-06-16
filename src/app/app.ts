beac5a6919059e74690bc9b2f6a7200d4ebea221
nika97z <zirakishvili3@gmail.com>
2026-06-16 12:11:29 +0400

    last

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, inject, signal, PLATFORM_ID } from '@angular/core';
+import { Component, inject, signal, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { Footer } from './footer/footer';
@@ -11,19 +12,19 @@ import { createChat } from '@n8n/chat';
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App {
-
-  ngOnInit(){
-    createChat({
-      webhookUrl: 'https://zirakishvili3.app.n8n.cloud/webhook/165546a8-ea4b-4e8e-a2f4-28f5c5c96f98/chat'
-    })
-  }
-
+export class App implements AfterViewInit {
   protected readonly title = signal('resturant');
+  private platformId = inject(PLATFORM_ID);
 
   constructor() {
     inject(AuthService).checkAuth();
   }
 
-  
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      createChat({
+        webhookUrl: 'https://zirakishvili3.app.n8n.cloud/webhook/165546a8-ea4b-4e8e-a2f4-28f5c5c96f98/chat'
+      });
+    }
+  }
 }
